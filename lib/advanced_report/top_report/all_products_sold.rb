31ec73d478eb34e0e523b2bc781737a0e60ca7e0
Gary Taylor <gary.taylor@hismessages.com>
2011-10-14 13:34:05 +0100

    Added shipping

--- lib/advanced_report/top_report/all_products_sold.rb
+++ lib/advanced_report/top_report/all_products_sold.rb
@@ -9,8 +9,9 @@ class AdvancedReport::TopReport::AllProductsSold < AdvancedReport::TopReport
 
   def initialize(params)
     super(params)
-
+    adjustments=0
     orders.each do |order|
+      adjustments+=order.adjustment_total.to_f
       order.line_items.each do |li|
         if !li.product.nil?
           data[li.product.id] ||= {
@@ -28,6 +29,7 @@ class AdvancedReport::TopReport::AllProductsSold < AdvancedReport::TopReport
     data.inject({}) { |h, (k, v) | h[k] = v[:revenue]; h }.sort { |a, b| a[1] <=> b [1] }.reverse.each do |k, v|
       ruportdata << { "name" => data[k][:name], "Units" => data[k][:units], "Revenue" => data[k][:revenue] } 
     end
+    ruportdata << { "name" => "Adjustments (Shipping etc.)", "Units" => orders.count, "Revenue" => adjustments}
     ruportdata.replace_column("Revenue") { |r| "£%0.2f" % r.Revenue }
     ruportdata.rename_column("name", "Product Name")
   end
