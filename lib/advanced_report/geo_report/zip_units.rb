fd3aae770b737dcc53814c1c4e256b8d2b002c4b
Gary Taylor <gary.taylor@hismessages.com>
2011-10-14 12:03:28 +0100

    Slight tweak to data output so that zip, county and state are available

--- lib/advanced_report/geo_report/zip_units.rb
+++ lib/advanced_report/geo_report/zip_units.rb
@@ -14,16 +14,24 @@ class AdvancedReport::GeoReport::ZipUnits < AdvancedReport::GeoReport
   def initialize(params)
     super(params)
 
-    data = { :state => {}, :country => {} }
+    data = { :zip => {}, :country => {} }
     orders.each do |order|
       units = units(order)
+      if order.bill_address and order.bill_address.state
+        data[:state][order.bill_address.state_id] ||= {
+          :name => order.bill_address.state.name,
+          :units => 0
+        }
+        data[:state][order.bill_address.state_id][:units] += units
+      end
+
       if order.bill_address and order.bill_address.zipcode
         zip=order.bill_address.zipcode.upcase.strip[0..1]
-        data[:state][zip] ||= {
+        data[:zip][zip] ||= {
           :name => zip,
           :units => 0
         }
-        data[:state][zip][:units] += units
+        data[:zip][zip][:units] += units
       end
       if order.bill_address and order.bill_address.country
         data[:country][order.bill_address.country_id] ||= {
@@ -34,7 +42,7 @@ class AdvancedReport::GeoReport::ZipUnits < AdvancedReport::GeoReport
       end
     end
 
-    [:state, :country].each do |type|
+    [:zip, :country].each do |type|
       ruportdata[type] = Table(%w[location Units])
       data[type].each { |k, v| ruportdata[type] << { "location" => v[:name], "Units" => v[:units] } }
       ruportdata[type].sort_rows_by!(["location"])
